44863934e29e82a094391317ac83bc3f323f379e
Gurdit <16bitmood@gmail.com>
2021-04-25 01:52:57 +0530

    feature: added basic login/logout functionality and hooked it up to backend

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,43 +1,43 @@
-import axios from 'axios';
+import axios from "axios";
 
 export class Unauthorized extends Error {
-    constructor(msg = 'Unauthorized') {
-        super(msg);
-    }
+  constructor(msg = "Unauthorized") {
+    super(msg);
+  }
 }
 
 export class BadRequest extends Error {
-    constructor(msg = 'Bad Request') {
-        super(msg);
-    }
+  constructor(msg = "Bad Request") {
+    super(msg);
+  }
 }
 
-export async function logIn(username: string, password: string): Promise<boolean> {
-    const res = await axios.post(
-        '/api/login',
-        { username, password }
-    );
+export async function logIn(
+  username: string,
+  password: string
+): Promise<boolean> {
+  const res = await axios.post("/api/login", { username, password });
 
-    return res.status === 200;
+  return res.status === 200;
 }
 
 export async function logOut(): Promise<boolean> {
-    const res = await axios.post('/api/logout');
-    return res.status === 200;
+  const res = await axios.post("/api/logout");
+  return res.status === 200;
 }
 
 export async function isLoggedIn(): Promise<boolean> {
-    const res = await axios.post('/api/isLoggedIn');
-    return res.data.result === 'yes';
+  const res = await axios.get("/api/isLoggedIn");
+  return res.data.result === "yes";
 }
 
 export async function createApp(appName: string): Promise<number> {
-    const res = await axios.post(`/api/manage/create/${appName}`);
-    if (res.status === 200) {
-        return res.data.appId
-    } else if (res.status === 401) {
-        throw new Unauthorized();
-    } else {
-        throw new BadRequest();
-    }
+  const res = await axios.post(`/api/manage/create/${appName}`);
+  if (res.status === 200) {
+    return res.data.appId;
+  } else if (res.status === 401) {
+    throw new Unauthorized();
+  } else {
+    throw new BadRequest();
+  }
 }
---
+++ client/src/app.tsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+
+import { logIn, logOut, isLoggedIn as isAuthenticated } from "./api";
+
+const LoadingScreen = () => <>Loading</>;
+
+const Home = (props: { onLogout: () => void }) => {
+    const handleClick: React.MouseEventHandler = (ev) => {
+        props.onLogout();
+        logOut(); // A bit Wonky
+    };
+
+    return (
+    <>
+      <h1>Login Successful</h1>
+      <button onClick={handleClick}>Logout</button>
+    </>
+    );
+};
+
+const LoginForm = (props: { onLogin: () => void }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [incorrectAttempt, setIncorrectAttempt] = useState(false);
+
+  const handleSubmit = async (ev: any) => {
+    ev.preventDefault();
+    const isAuthenticated = await logIn(username, password);
+    if (isAuthenticated) {
+        props.onLogin();
+    } else {
+        setIncorrectAttempt(true);
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>Username:</p>
+          <input type="text" onChange={(ev) => setUsername(ev.target.value)} />
+        </label>
+        <label>
+          <p>Username:</p>
+          <input
+            type="password"
+            onChange={(ev) => setPassword(ev.target.value)}
+          />
+        </label>
+        <div>
+          <button type="submit"> Submit </button>
+        </div>
+      </form>
+      {incorrectAttempt ? "Incorrect Credentials!" : ""}
+    </>
+  );
+};
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean | null>(null);
+
+  useEffect( () => {
+      isAuthenticated().then((status) => setIsLoggedIn(status));
+  },[]);
+
+  if (isLoggedIn === null) {
+    return <LoadingScreen />;
+  } else if (isLoggedIn) {
+    return <Home onLogout={() => setIsLoggedIn(false)} />;
+  } else {
+    return <LoginForm onLogin={() => setIsLoggedIn(true)} />;
+  }
+}
+
+export default App;
