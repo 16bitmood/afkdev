3e42f83c982caeca3be6d2c4d1ee601bad2e7d08
Gurdit <16bitmood@gmail.com>
2021-05-01 19:56:35 +0530

    fix: show localhost on stats

--- server/src/routes/stats.ts
+++ server/src/routes/stats.ts
@@ -3,18 +3,17 @@ import { Router } from "express";
 import { isLoggedIn } from "../session";
 import { BadRequest } from "../errors";
 
-import { makeApp } from "../webapps";
-
-import { setSessionApp } from "../session";
-
 const router = Router();
 
 router.get("/stats", (req, res) => {
   if (!isLoggedIn(req)) {
-    throw new BadRequest("Client must be logged in to access /statistics");
+    throw new BadRequest("Client must be logged in to access /stats");
   }
 
-  res.json({ 'ip': req.ip });
+  res.json({
+      'ip': (req.ip === '::ffff:127.0.0.1' || req.ip === '::1') ? 'localhost' : req.ip,
+  });
+
 });
 
 export { router as stats };
