127dfdb88b2da55a9abe2aca96c25c022c395dae
Gurdit <16bitmood@gmail.com>
2021-05-03 19:41:14 +0530

    feature: page transitions

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,6 +1,7 @@
 import "./styles/global.scss";
 
-import { FC, useContext, useEffect } from "react";
+import { FC, useState, useContext, useEffect } from "react";
+import { CSSTransition, SwitchTransition } from "react-transition-group";
 
 import { isLoggedIn as isAuthenticated } from "./api";
 import { SessionContext, SessionContextProvider } from "./contexts";
@@ -11,22 +12,47 @@ import { LoadingPage } from "./containers/loading";
 
 const HandlePages: FC = () => {
   const { isLoggedIn, setIsLoggedIn } = useContext(SessionContext);
+  const [currentPage, setCurrentPage] = useState(isLoggedIn ? 3 : 1);
 
   useEffect(() => {
     const checkLogin = async () => {
       const r = await isAuthenticated();
       setIsLoggedIn(r);
+      if (r) {
+        setCurrentPage(3);
+      } else {
+        setCurrentPage(2);
+      }
+      console.log(isLoggedIn, "changed");
     };
     checkLogin();
-  }, []);
+  }, [isLoggedIn]);
 
-  if (isLoggedIn === null) {
-    return <LoadingPage />;
-  }
-  if (isLoggedIn) {
-    return <HomePage />;
-  }
-  return <LoginPage />;
+  const getPage = (id: number) => {
+    switch (id) {
+      case 1:
+        return <LoadingPage />;
+      case 2:
+        return <LoginPage />;
+      case 3:
+        return <HomePage />;
+      default:
+        throw new Error("");
+    }
+  };
+
+  return (
+    <SwitchTransition>
+      <CSSTransition
+        key={currentPage}
+        unmountOnExit
+        timeout={500}
+        classNames={currentPage !== 3 ? "fade-transition" : "expand-transition"}
+      >
+        {getPage(currentPage)}
+      </CSSTransition>
+    </SwitchTransition>
+  );
 };
 
 const App: FC = ({ children }) => (
